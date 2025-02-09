5329cfda973e800fa20b87918b6d42fb6392d00f
luckcapsule <86635031+luckcapsule@users.noreply.github.com>
2025-02-09 00:22:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 FlexMasonry.init(".img-gallery", {
   breakpointCols: {
-    "min-width: 1024px": 4,
-    "min-width: 768px": 3,
-    "min-width: 540px": 2
+    "min-width: 1024px": 3,
+    "min-width: 768px": 2,
+    "min-width: 540px": 1
   }
 });
